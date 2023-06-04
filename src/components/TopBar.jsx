e38cb0ed6c8f5e1045f71f95cafe653b590e802a
Seamoon Pandey <hackedasocial@gmail.com>
2023-06-04 17:05:25 +0545

    topbar complete

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+    import React,{ useState } from 'react'
+    import './topbar.css'
+    import { List, X } from 'phosphor-react';
 
-export default function TopBar() {
-  return (
-    <header>
-        <div>logo</div>
-    </header>
-  )
-}
+    export default function Topbar() {
+        const [isNavExpanded, setIsNavExpanded] = useState(false)
+        return (
+        <nav className="navigation">
+            <a href="/" className="brand-name">
+            Pachyand
+            </a>
+            <div>
+            <button className="hamburger"  onClick={() => {
+            setIsNavExpanded(!isNavExpanded)
+            }}>
+            {!isNavExpanded?<List size={32}/>:<X size={32}/>}
+            </button>
+            <div
+            className={
+            isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+            }
+        >
+            <ul>
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="#judges">Judges</a>
+            </li>
+            <li>
+                <a href="#prizes">Prize Pool</a>
+            </li>
+            {/* <li>
+                <a href="#"></a>
+            </li> */}
+            <li>
+                <a href="#our-sponsors">Sponsors</a>
+            </li>
+            <li>
+                <a href="#faqs">FAQ's</a>
+            </li>
+            </ul>
+        </div>
+            </div>
+        </nav>
+        );
+    }
