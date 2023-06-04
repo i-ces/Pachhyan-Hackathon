71347938a27d43beac601fe8aba390d1ab94196e
Seamoon Pandey <hackedasocial@gmail.com>
2023-06-05 00:23:44 +0545

    add cards and some minor changes

--- src/components/JudgePanel.jsx
+++ src/components/JudgePanel.jsx
@@ -1,16 +1,49 @@
-import React from 'react'
-import "./judgepanel.css"
-import Title from './subcomponents/Title'
+import React from 'react';
+import './judgepanel.css';
+import Title from './subcomponents/Title';
+import Card from './subcomponents/Card';
 
 const JudgePanel = () => {
-  return (
-    <div>
-      <Title text="Judge Panel"/>
-      <div className='judgepanel_main'>
-      something good here
-    </div>
-    </div>
-  )
-}
+  const mentorsData = [
+    {
+      id: 1,
+      name: 'John Doe',
+      position: 'Senior Developer',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+   avatar:'/images/avatar.jpg'
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      position: 'UX Designer',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      avatar:'/images/avatar.jpg'
+    },
+    {
+      id: 3,
+      name: 'Mike Johnson',
+      position: 'Data Scientist',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      avatar:'/images/avatar.jpg'
+    },
+  ];
 
-export default JudgePanel
+  return (
+    <div className='mentors'>
+      <Title text="Mentors" />
+      <div className='mentors-main'>
+        {mentorsData.map((mentor) => (
+          <Card
+            key={mentor.id}
+            name={mentor.name}
+            position={mentor.position}
+            description={mentor.description}
+            avatar={mentor.avatar}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default JudgePanel;
--- src/components/Prizepool.jsx
+++ src/components/Prizepool.jsx
@@ -10,12 +10,14 @@ export default function Prizepool() {
     <div className="prizepool-container">
     <div className="prize-container">
         <img src="/images/Award.png" alt="cup1" className="prize-image-1" />
+        <div className='prize-subtitle'>Grand Prize</div>
         <div className="price-tag">
           <Price amount={25000} color={"#ffc107"}/>
         </div>
       </div>
       <div className="prize-container">
         <img src="/images/Second.png" alt="cup2" className="prize-image-2" />
+        <div className='prize-subtitle'>Runner Up</div>
         <div className="price-tag">
          <Price amount={10000} color={"silver"}/>
         </div>
