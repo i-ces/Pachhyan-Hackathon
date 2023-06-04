16b2cc07dd64dd9a96d69961cca63e932c929061
Seamoon Pandey <hackedasocial@gmail.com>
2023-06-05 01:27:46 +0545

    final touch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import TopBar from './components/TopBar'
-import Rules from './components/Rules'
-import Prizepool from './components/Prizepool'
-import MainContent from './components/MainContent'
-import JudgePanel from './components/JudgePanel'
-import FAQs from './components/FAQs'
+import React from 'react';
+import TopBar from './components/TopBar';
+import Rules from './components/Rules';
+import Prizepool from './components/Prizepool';
+import MainContent from './components/MainContent';
+import JudgePanel from './components/JudgePanel';
+import FAQs from './components/FAQs';
 
 export default function App() {
   return (
-    <>
-    <TopBar/>
-    <MainContent/>
-    <JudgePanel/>
-    <Prizepool/>
-    <Rules/>
-    <FAQs/>
-    </>
-  )
+    <div className="app-container">
+      <TopBar />
+      <div className="content-wrapper">
+        <div><MainContent /></div>
+        <div><JudgePanel /></div>
+        <div><Prizepool /></div>
+        <div><Rules /></div>
+        <div><FAQs /></div>
+      </div>
+    </div>
+  );
 }
