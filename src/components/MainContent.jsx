6b5e158cc13e3e1728a88966ba2cd2bfaf3fa3c6
paraspoudel72 <pas076bct020@wrc.edu.np>
2023-06-05 01:55:40 +0545

    merged

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import './main.css';
 
 export default function MainContent() {
   return (
-    <div className="main-content-container">
-      <div className="main-content">
-        <div className="partners-row">
-          <div className="partner">
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col items-center text-center">
+        <div className="flex justify-center gap-20">
+          <div className="flex items-center justify-center w-24 h-24">
             <img src="/images/data.png" alt="something1" height={100} width={100} />
           </div>
-          <div className="logo">Logo part</div>
-          <div className="partner">
+          <div className="flex justify-center items-center w-24 h-24 border-dashed border">
+            Logo part
+          </div>
+          <div className="flex items-center justify-center w-24 h-24">
             <img src="/images/data.png" alt="something1" height={100} width={100} />
           </div>
         </div>
-        <div className="title-main">Pachyaan Hackathon</div>
-        <div className="description">
+        <div className="mt-8 text-4xl font-bold">Pachyaan Hackathon</div>
+        <div className="mt-4 max-w-2xl px-8">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quibusdam inventore dolore provident.
             Iste cupiditate doloribus labore! Nisi excepturi, veritatis facilis rem voluptates tempore aliquid quo ipsum
