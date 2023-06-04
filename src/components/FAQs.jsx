513f1d785e416fdad67d015be89971c177943ad3
Seamoon Pandey <hackedasocial@gmail.com>
2023-06-05 01:21:47 +0545

    faqs add

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Accordion from './subcomponents/Accordion';
+import faqsData from '../store/faq.json'
+import Title from './subcomponents/Title';
+import './faqs.css'
 
 export default function FAQs() {
+
   return (
-    <div>FAQs</div>
-  )
+    <div className='faq'>
+      <Title text="FAQs"/>
+    <div className='faq-list'>
+    {faqsData.map((faq) => (
+        <Accordion
+          key={faq.id}
+          title={faq.question}
+          description={faq.answer}
+        />
+      ))}
+    </div>
+    </div>
+  );
 }
