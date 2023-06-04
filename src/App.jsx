3c1e3ab8813ddcdb698ab625c55fd3f4798a7140
Seamoon Pandey <hackedasocial@gmail.com>
2023-06-04 17:21:14 +0545

    done rules with dummy text

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import TopBar from './components/TopBar'
+import Rules from './components/Rules'
 
 export default function App() {
   return (
+    <>
     <TopBar/>
+    <Rules/>
+    </>
   )
 }
---
+++ src/components/subcomponents/Title.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './title.css';
+
+export default function Title({ text }) {
+  return <div className='title'>{text}</div>;
+}
