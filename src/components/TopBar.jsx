064ce0b9dc65ee76a50c52305265a6780113389d
Seamoon Pandey <hackedasocial@gmail.com>
2023-06-05 09:18:26 +0545

    ui fixes

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,47 +1,45 @@
-    import React,{ useState } from 'react'
-    import './topbar.css'
-    import { List, X } from 'phosphor-react';
+import React, { useState } from 'react';
+import { List, X } from 'phosphor-react';
+import './topbar.css';
 
-    export default function Topbar() {
-        const [isNavExpanded, setIsNavExpanded] = useState(false)
-        return (
-        <nav className="navigation">
-            <a href="/" className="brand-name">
-            Pachhyan Hackathon 
-            </a>
-            <div>
-            <button className="hamburger"  onClick={() => {
-            setIsNavExpanded(!isNavExpanded)
-            }}>
-            {!isNavExpanded?<List size={32}/>:<X size={32}/>}
-            </button>
-            <div
-            className={
-            isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-            }
+export default function TopBar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
+
+  return (
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        Pachhyan Hackathon
+      </a>
+      <div>
+        <button className="hamburger" onClick={toggleNav}>
+          {!isNavExpanded ? <List size={32} /> : <X size={32} />}
+        </button>
+        <div
+          className={`navigation-menu ${isNavExpanded ? 'expanded' : ''}`}
         >
-            <ul>
-            <li>
-                <a href="#home">Home</a>
+          <ul>
+            <li className="nav-link">
+              <a href="#home">Home</a>
             </li>
-            <li>
-                <a href="#judges">Judges</a>
+            <li className="nav-link">
+              <a href="#prizes">Prize Pool</a>
             </li>
-            <li>
-                <a href="#prizes">Prize Pool</a>
+            <li className="nav-link">
+              <a href="#sponsors">Sponsors</a>
             </li>
-            {/* <li>
-                <a href="#"></a>
-            </li> */}
-            <li>
-                <a href="#our-sponsors">Sponsors</a>
+            <li className="nav-link">
+              <a href="#rule-book">Code of Conduct</a>
             </li>
-            <li>
-                <a href="#faqs">FAQ's</a>
+            <li className="nav-link">
+              <a href="#faqs">FAQs</a>
             </li>
-            </ul>
+          </ul>
         </div>
-            </div>
-        </nav>
-        );
-    }
+      </div>
+    </nav>
+  );
+}
--- src/components/subcomponents/Title.jsx
+++ src/components/subcomponents/Title.jsx
@@ -4,6 +4,6 @@ import './title.css';
 export default function Title({ text }) {
   return <div className='title flex-row '>
     {text}
-    <hr className="w-2/3 border-t-2 border-black rounded-full border-2 mx-auto border-[#A6B1BB]" />
+    <hr className="w-2/3 border-t-2 border-[#6AE58E] rounded-full border-2 mx-auto border-[#6AE58E]" />
     </div>;
 }
