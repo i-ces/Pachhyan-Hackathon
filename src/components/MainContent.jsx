58da172c71adf986451e0177e698605f78e499f6
Shishir Rijal <ccrrizal0892@gmail.com>
2023-06-05 15:23:54 +0545

    padding fix on rules and guidelines card

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -16,12 +16,18 @@ export default function MainContent() {
           </div>
         </div>
         <div className="mt-8 text-4xl font-bold">Pachhyan Hackathon</div>
-        <div className="mt-4 max-w-2xl px-8">
-          <p>
-          Pachhyan hackathon is a 48 hour event on the theme “Culture and Tourism: Unleashing Technology's Potential”  with the aim of fostering innovation and promoting the cultural heritage and tourism potential. In today's rapidly evolving world, technology has become an integral part of our daily lives, shaping industries and transforming the way we interact with the world around us. The culture and tourism sectors are no exception, with technology offering endless possibilities for enhancing visitor experiences, preserving cultural heritage, and promoting sustainable tourism practices. This  hackathon seeks to harness the creative power of technology enthusiasts, developers, designers, and entrepreneurs to ideate, collaborate, and develop innovative solutions that will revolutionize the culture and tourism landscape.
+        <div className="mt-4 max-w-4xl px-8">
+          <p >
+            Pachhyan hackathon is a 48 hour event on the theme “Culture and Tourism: Unleashing Technology's Potential”  with the aim of fostering innovation and promoting the cultural heritage and tourism potential. In today's rapidly evolving world, technology has become an integral part of our daily lives, shaping industries and transforming the way we interact with the world around us. The culture and tourism sectors are no exception, with technology offering endless possibilities for enhancing visitor experiences, preserving cultural heritage, and promoting sustainable tourism practices. This  hackathon seeks to harness the creative power of technology enthusiasts, developers, designers, and entrepreneurs to ideate, collaborate, and develop innovative solutions that will revolutionize the culture and tourism landscape.
           </p>
         </div>
+        <button >
+          <span className='bg-blue border-white'>
+            Register now
+          </span>
+        </button>
       </div>
+
     </div>
   );
 }
