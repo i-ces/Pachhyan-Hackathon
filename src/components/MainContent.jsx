9a130c1d945296bc3e5432cf029085dc5337a9d5
paraspoudel72 <pas076bct020@wrc.edu.np>
2023-06-05 20:50:55 +0545

    just minor changes

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -16,7 +16,7 @@ export default function MainContent() {
           </div>
         </div>
         <div className="mt-8 text-4xl font-bold">Pachhyan Hackathon</div>
-        <div className="mt-4 max-w-2xl px-8">
+        <div className="mt-4 max-w-2xl px-8 text-lg">
           <p>
           Pachhyan hackathon is a 48 hour event on the theme “Culture and Tourism: Unleashing Technology's Potential”  with the aim of fostering innovation and promoting the cultural heritage and tourism potential. In today's rapidly evolving world, technology has become an integral part of our daily lives, shaping industries and transforming the way we interact with the world around us. The culture and tourism sectors are no exception, with technology offering endless possibilities for enhancing visitor experiences, preserving cultural heritage, and promoting sustainable tourism practices. This  hackathon seeks to harness the creative power of technology enthusiasts, developers, designers, and entrepreneurs to ideate, collaborate, and develop innovative solutions that will revolutionize the culture and tourism landscape.
           </p>
--- src/components/subcomponents/Title.jsx
+++ src/components/subcomponents/Title.jsx
@@ -4,6 +4,6 @@ import './title.css';
 export default function Title({ text }) {
   return <div className='title flex-row '>
     {text}
-    <hr className="w-2/3 border-t-2 border-[#6AE58E] rounded-full border-2 mx-auto border-[#6AE58E]" />
+    <hr className="w-2/3 border-t-2 rounded-full border-2 mx-auto border-[#6AE58E]" />
     </div>;
 }
