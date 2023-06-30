7ce46b8598fa2892a965369185e8446e96f9c150
Seamoon Pandey <115852972+seamoonpandey@users.noreply.github.com>
2023-06-30 06:04:46 +0545

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -26,9 +26,9 @@ export default function MainContent() {
             <b>EthosHack</b> is a 24-hour hackathon organized by Sudur Paschim Samparka Samaj and Pashchimanchal Campus, in collaboration with the Innovative Computer Engineering Students' Society. It brings together Nepalese students with a shared interest in technology, culture, and innovation. The hackathon aims to celebrate and preserve Nepal's diverse cultural heritage by combining technology and innovation. Participants are encouraged to create projects that showcase the cultural richness, traditions, and attractions of different regions of Nepal. EthosHack provides a platform for collaboration, idea generation, and development of software, applications, and solutions.</p>
         </div>
         <div className="bg-blue p-6">
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSfYav028UepPC0EVpm6qqMUct8W1lPRBEQ9IJTnggdGFkOwvA/viewform" target="blank">
-            <button>Register Now</button>
-          </a>
+          <div>
+            <button>We are Closed Now</button>
+          </div>
         </div>
       </div>
     </div>
